2667f522dd6b32fbb1c2edea1ba0534b24248f54
barwen1899 <bartek.jj@gmail.com>
2020-06-02 10:40:46 +0100

    Creating cpp game. Drawing player sprite. Implemented moving the player via keyboard.

---
+++ Engine/Player.cpp
@@ -0,0 +1,15 @@
+#include "Player.h"
+
+
+
+Player::Player(const int&x, const int&y)
+{
+	xCord = x;
+	yCord = y;
+}
+
+
+Player::~Player()
+{
+
+}
---
+++ Engine/Player.h
@@ -0,0 +1,15 @@
+#pragma once
+class Player
+{
+public:
+
+	Player(const int&x, const int&y);
+
+	~Player();
+
+private:
+
+	//Location
+	int xCord, yCord;
+};
+
